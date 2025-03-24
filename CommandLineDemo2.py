9e52c359dbcf35be401612fc2578c535978998f4
Sanketh-2004 <245122748045@mvsrec.edu.in>
2025-03-24 13:31:35 +0530

    CommandLineDemo2.py

--- CommandLineDemo2.py
+++ CommandLineDemo2.py
@@ -1,11 +1,7 @@
 import sys
-s1=int(sys.argv[1])
-s2=int(sys.argv[2])
-s3=int(sys.argv[3])
-r1=n1+n2
-r2=n1-n2
-r3=n1*n2
-r4=n1/n2
+s1=sys.argv[1]
+s2=sys.argv[2]
+s3=sys.argv[3]
 
 print("File:",sys.argv[0])
 print("Name:",s1)
